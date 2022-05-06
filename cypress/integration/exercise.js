bbf65d6570b187c0913f8805f9fa6dff13a0c829
Tejaswini Dhusa <tejaswini@Tejaswinis-MacBook-Pro.local>
2022-05-06 18:18:32 -0400

    added tests for the index page

--- cypress/integration/exercise.js
+++ cypress/integration/exercise.js
@@ -7,4 +7,30 @@ describe("verify index page", () => {
     //To start tests of index page, first we want to load the index page
     cy.visit("http://localhost:3000/");
   });
+
+  it("should have app logo by default", () => {
+    //This test verifies the default app logo of index page
+    cy.get(".App-logo")
+      .should("be.visible") //app logo should be visible
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0); //Width of the logo should be greater than zero
+      });
+  });
+  it("should have edit index page text", () => {
+    //This test verifies the page should have given edit text
+    cy.get("p").should("have.text", "Edit src/App.js and save to reload.");
+  });
+
+  it("should have hyperlink with text", () => {
+    //This test verifies the given hyperlink on index page
+    cy.get("a:visible")
+      .contains("Learn React")
+      .should("have.attr", "href", "https://reactjs.org");
+  });
+
+  it("hyperlink should be working", () => {
+    //This test verifies the functionality of hyperlink
+    cy.get("a:visible").invoke("removeAttr", "target").click(); //click action verified
+    cy.url().should("include", "reactjs.org"); //url verified
+  });
 });
